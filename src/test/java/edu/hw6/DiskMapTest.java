3acdd3c1a42586683bb5b826127806163946bc90
evgeniiprotsenko <procenko.zheka@gmail.com>
2023-11-20 01:54:40 +0500

    hw6. v2

--- src/test/java/edu/hw6/DiskMapTest.java
+++ src/test/java/edu/hw6/DiskMapTest.java
@@ -10,40 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class DiskMapTest {
 
     @Test
-    public void testDiskMapOperations() {
-
-        Path path = Paths.get("src/test/java/edu/hw6/Task1/res.txt");
-        Path path2 = Paths.get("src/test/java/edu/hw6/Task1/res2.txt");
-
-        try {
-            Files.deleteIfExists(path);
-            Files.deleteIfExists(path2);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        DiskMap diskMap = new DiskMap(path.toString());
-
-        // Проверка добавления элементов
-        diskMap.put("key1", "val1");
-        diskMap.put("key2", "val2");
-        assertEquals("[key1=val1, key2=val2]", diskMap.entrySet().toString());
-
-        // Проверка удаления элемента
-        diskMap.remove("key1");
-        assertEquals("[key2=val2]", diskMap.entrySet().toString());
-
-        // Проверка сохранения в новый файл
-        diskMap.saveToFile(path2.toString());
-        diskMap.put("key3", "val3");
-        assertEquals("[key2=val2, key3=val3]", diskMap.entrySet().toString());
-
-        // Проверка загрузки из другого файла
-        diskMap.loadFromFile(path.toString());
-        assertEquals("[key2=val2]", diskMap.entrySet().toString());
-    }
-
-    @Test
     public void testPutAndGet() {
         Path path = Paths.get("src/test/java/edu/hw6/Task1/res.txt");
 
